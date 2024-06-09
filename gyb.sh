c6962be599df8de69441c20abb08c530c98b3caa
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2024-06-09 13:36:10 -0500

    Support GYB's two-legged auth flow.
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- gyb.sh
+++ gyb.sh
@@ -33,16 +33,25 @@ then
   touch "${CREDENTIALS_FOLDER}/client_secrets.json"
   touch "${CREDENTIALS_FOLDER}/email.cfg"
   >&2 echo "INFO: You need to set up GYB. Let's do that first."
+  >&2 echo "INFO: You'll probably get an error message in your browser. That's okay. Copy \
+the URL from the address bar and paste it into the prompt shown below to continue setup."
 
   # We have to copy the client_secrets.json file out of the container because gyb
   # refuses to create a new project if this file exists but Docker will create
   # the file as a directory if the file used by the volume mount doesn't exist.
   # https://github.com/jay0lee/got-your-back/blob/master/gyb.py#L911-L913
-  docker-compose -f "$(dirname $0)/docker-compose.yml" run --user root \
+  if ! docker-compose -f "$(dirname $0)/docker-compose.yml" run --user root \
+    --rm \
+    --service-ports \
     gyb-without-project \
       "$(echo "./gyb --email ${email_address} \
 --local-folder /tmp/local_folder \
 --action create-project" | base64 -)"
+  then
+    >&2 echo "ERROR: Failed to set up GYB."
+    rm -rf "${CREDENTIALS_FOLDER}"
+    exit 1
+  fi
 
   docker ps -a | \
     grep gyb-without-project | \
@@ -59,6 +68,8 @@ docker exec {} sh -c 'cat /usr/local/bin/gyb/client_secrets.json'" > "${CREDENTI
 docker exec {} sh -c 'cat /usr/local/bin/gyb/oauth2service.json' " > "${CREDENTIALS_FOLDER}/oauth2service.json"
 fi
 
-docker-compose -f "$(dirname $0)/docker-compose.yml" run --user root \
+docker-compose -f "$(dirname $0)/docker-compose.yml" run \
+  --rm \
+  --user root \
   gyb \
   "$(echo "./gyb --email ${email_address} --local-folder /tmp/local_folder ${@:1}" | base64 -)"
