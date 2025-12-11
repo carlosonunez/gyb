e223bac63a5eaa6f3b5a4280788836cdf830a1e9
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2025-12-11 13:30:11 -0600

    [fix] handle quotes in gyb commands better
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- run_gyb.sh
+++ run_gyb.sh
@@ -2,4 +2,4 @@
 base64_encoded_command="${1?Please provide a command; ensure that it is base64 encoded.}"
 
 echo "Running: $(echo "$base64_encoded_command" | base64 -d -)"
-eval $(echo "$base64_encoded_command" | base64 -d -)
+eval "$(echo "$base64_encoded_command" | base64 -d -)"
